08ad1d0aec6714e19f16e395a3796d9e89f38a5b
Sayem <sayem@gmail.com>
2021-05-24 13:01:53 +0600

    Fix url

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,7 +1,7 @@
 // url
 let Url = ''
 if (process.env.NODE_ENV === "production") {
-     Url = "https://searchtrack-demo.herokuapp.com/"
+     Url = "https://searchtrack-demo.herokuapp.com"
 } else {
     Url =  "http://127.0.0.1:8000"
 }
